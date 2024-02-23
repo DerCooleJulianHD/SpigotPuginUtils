0dae7140bbaa7298dac67a66450d4cf0f0aba921
DerCooleJulianHD YT <109760232+DerCooleJulianHD@users.noreply.github.com>
2024-02-23 15:44:03 +0100

    Update Configuration.java

--- Configuration.java
+++ Configuration.java
@@ -8,80 +8,94 @@ import org.bukkit.plugin.Plugin;
 
 import java.io.File;
 
-public class Configuration { // Type: YAML-Configuration //
+public class Configuration /* Type: YAML-Configuration */ {
 
-    // Attribute
     private final Plugin plugin;
     private File file;
     private YamlConfiguration yamlConfiguration;
     private boolean loaded, exist;
 
 
-    // creating new Config File with Directory File//
-    public Configuration(Plugin plugin, File directory, String name) { // File-Name without ending, only name//
+    public Configuration(Plugin plugin, File directory, String name/* Name from File which will be created! */) {
+        
         this.plugin = plugin;
-        if (!directory.exists()){ // check directory path existing //
-            // Directory doesn't found //
-            ConsoleCommandSender console = Bukkit.getServer().getConsoleSender();
-            console.sendMessage(plugin.getName() + ChatColor.RED + "The specified folder path doesn't exist!");
+        
+        if (!directory.exists() /* check directory path existing */ ){ 
+            /* Directory wasn't found */
+            Bukkit.getServer().getConsoleSender().sendMessage(plugin.getName() + ChatColor.RED + "The specified folder path doesn't exist!");
             return;
         }
-        // Directory Found //
+        
+        /* Directory Found */
         this.file = new File(directory, name + ".yml");
+        
         this.exist = (directory.exists() && file.exists());
         this.loaded = false;
     }
 
-    public void createNewConfiguration(){
-        if (file.exists()) return; // check file existing //
-        // Creating Config File //
+    public void createNewConfiguration() /* creates Configuration-File if it doesn't exist */ {
+        
+        if (file.exists()) return; /* check file existing */
+    
         try {
-            file.createNewFile();
+            file.createNewFile(); /* Creating Config File */
         } catch (Exception exception) {
-            // Config File couldn't be created //
-            ConsoleCommandSender console = Bukkit.getServer().getConsoleSender();
-            console.sendMessage(plugin.getName() + ChatColor.RED + "Couldn't create file: " + file.getName() + ": " + exception.getCause());
+            /* Config File couldn't be created */
+            Bukkit.getServer().getConsoleSender().sendMessage(plugin.getName() + ChatColor.RED + "Couldn't create file: " + file.getName() + ": " + exception.getCause());
         }
+        
     }
 
-    public void loadConfiguration(){ // Load Config File //
-        if (isLoaded()) return;
-        try {
+    public void loadConfiguration() /* loads configuration */ {
+        
+        if (isLoaded() /* check if the Configuration is already loaded */ ) return;
+        
+        try { /* load */
             this.yamlConfiguration = YamlConfiguration.loadConfiguration(file);
-            this.loaded = true;
-        } catch (Exception exception){
-            ConsoleCommandSender console = Bukkit.getServer().getConsoleSender();
-            console.sendMessage(plugin.getName() + ChatColor.RED + "Couldn't load configuration: " + exception.getCause());
+            setLoaded(true);
+        } catch (Exception exception /* loading error */){
+            Bukkit.getServer().getConsoleSender().sendMessage(plugin.getName() + ChatColor.RED + "Couldn't load configuration: " + exception.getCause());
         }
+        
     }
 
-    public void save() {
+    public void save() /* saves configuration changes */ {
+        
         try {
-            // Save Configuration //
             yamlConfiguration.save(file);
         } catch (Exception exception) {
-            // Config File couldn't be saved //
-            ConsoleCommandSender console = Bukkit.getServer().getConsoleSender();
-            console.sendMessage(plugin.getName() + ChatColor.RED + "Couldn't save file: " + file.getName() + ": " + exception.getCause());
+            /* Config File couldn't be saved */
+            Bukkit.getServer().getConsoleSender().sendMessage(plugin.getName() + ChatColor.RED + "Couldn't save file: " + file.getName() + ": " + exception.getCause());
         }
+        
     }
 
-    public YamlConfiguration configuration() {
+    public Plugin getPlugin() {
+        return plugin;
+    }
+
+    public YamlConfiguration getConfiguration() {
         return yamlConfiguration;
     }
+    
     public boolean exists(){
         return exist;
     }
-    public Plugin plugin() {
-        return plugin;
+
+    private void setExists(boolean exist) {
+        this.exist = exist;
     }
+        
     public File getFile() {
         return file;
     }
+    
     public boolean isLoaded() {
         return loaded;
     }
+    
     public void setLoaded(boolean loaded) {
         this.loaded = loaded;
     }
+    
 }
