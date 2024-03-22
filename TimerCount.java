577e3fc12bc440c45b454c3e4b908c0966accb0d
DerCooleJulianHD YT <109760232+DerCooleJulianHD@users.noreply.github.com>
2024-03-22 14:55:13 +0100

    Delete TimerCount.java

--- TimerCount.java
+++
@@ -1,8 +0,0 @@
-package de.dercoolejulianhd.pluginutilities.timer;
-
-public enum TimerCount /* defines is which direction the Timer should count */ {
-
-    UP(), /* Count the declared time one up on each count */
-    DOWN(); /* Count the declared time one down on each count */
-
-}
