68fc319671a33a0166d8b1a264e2d4dc985e3ab7
DerCooleJulianHD YT <109760232+DerCooleJulianHD@users.noreply.github.com>
2024-06-07 14:20:55 +0200

    Add files via upload

---
+++ utils/scoreboard/LocalScoreboard.java
@@ -0,0 +1,15 @@
+package src.de.dercoolejulianhd.plugin.utils.scoreboard;
+
+import org.bukkit.plugin.Plugin;
+
+public class LocalScoreboard extends ScoreboardBuilder {
+
+    public LocalScoreboard(Plugin plugin, String displayName) {
+        super(plugin, displayName, true);
+    }
+
+    @Override
+    public void update(int score, String content) {
+        this.setScore(content, score);
+    }
+}
---
+++ utils/scoreboard/PlayerScoreboard.java
@@ -0,0 +1,24 @@
+package src.de.dercoolejulianhd.plugin.utils.scoreboard;
+
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.Plugin;
+
+public class PlayerScoreboard extends ScoreboardBuilder {
+
+    private final Player player;
+
+    public PlayerScoreboard(Plugin plugin, Player player, String displayName) {
+        super(plugin, displayName, false);
+        this.player = player;
+        player.setScoreboard(this.getScoreboard());
+    }
+
+    @Override
+    public void update(int score, String content) {
+        this.setScore(content, score);
+    }
+
+    public Player getPlayer() {
+        return player;
+    }
+}
